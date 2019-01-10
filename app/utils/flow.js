5a290d4184ffd35ea768cf12791db2cafebfa512
JordanVScher <jordanvscher@hotmail.com>
2019-01-10 17:54:56 -0200

    Fix aí

--- app/utils/flow.js
+++ app/utils/flow.js
@@ -2,7 +2,7 @@ module.exports = {
 	avatarImage: 'https://gallery.mailchimp.com/926cb477483bcd8122304bc56/images/c38095b8-6b51-46f9-a8a9-b61ac9ac9385.png',
 	getStarted: 'Sou a Mandy Selfie, a primeira assistente digital trans do Brasil. Clique em começar para falar comigo.',
 	greetings: {
-		text1: 'E ai, tudo bem?',
+		text1: 'E aí, tudo bem?',
 		text2: 'Sou a Mandy Selfie, a primeira assistente digital trans do Brasil. Estou aqui para falar dos bafos sobre sexo 😉',
 		text3: 'Você pode me mandar uma mensagem a qualquer momento que irei te responder ou se não souber encaminho para minha equipe e logo lhe responderemos ',
 	},
