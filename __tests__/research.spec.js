89a6313dad38f34476ab7e4d8b63b0b43b47f7c7
JordanVScher <jordanvscher@hotmail.com>
2019-02-05 12:14:12 -0200

    Add research text

---
+++ __tests__/research.spec.js
@@ -0,0 +1,49 @@
+const cont = require('./context');
+const research = require('../app/utils/research');
+const flow = require('../app/utils/flow');
+const opt = require('../app/utils/options');
+const desafio = require('../app/utils/desafio');
+
+jest.mock('../app/utils/flow');
+jest.mock('../app/utils/options');
+jest.mock('../app/utils/desafio');
+
+it('onTheResearch', async () => {
+	const context = cont.quickReplyContext('0', 'prompt');
+	await research.onTheResearch(context);
+
+	await expect(context.setState).toBeCalledWith({ dialog: 'onTheResearch' });
+	await expect(context.sendText).toBeCalledWith(flow.onTheResearch.text1);
+	await expect(context.sendImage).toBeCalledWith(flow.onTheResearch.gif);
+	await expect(context.sendText).toBeCalledWith(flow.onTheResearch.text2);
+});
+
+it('notOnResearch', async () => {
+	const context = cont.quickReplyContext('0', 'prompt');
+	await research.notOnResearch(context);
+
+	await expect(context.setState).toBeCalledWith({ dialog: 'NotOnResearch' });
+	await expect(context.sendText).toBeCalledWith(flow.NotOnResearch.text1);
+});
+
+it('notEligible', async () => {
+	const context = cont.quickReplyContext('0', 'prompt');
+	await research.notEligible(context);
+
+	await expect(context.setState).toBeCalledWith({ dialog: 'NotEligible' });
+	await expect(context.sendText).toBeCalledWith(flow.notEligible.text1, opt.saidNo);
+});
+
+it('researchSaidNo', async () => {
+	const context = cont.quickReplyContext('0', 'prompt');
+	await research.researchSaidNo(context);
+
+	await expect(context.sendText).toBeCalledWith(flow.quizNo.text1, opt.saidNo);
+});
+
+it('researchSaidYes', async () => {
+	const context = cont.quickReplyContext('0', 'prompt');
+	await research.researchSaidYes(context);
+
+	await expect(context.sendText).toBeCalledWith(flow.quizYes.text1, await desafio.checkAnsweredQuiz(context, opt.saidYes));
+});
