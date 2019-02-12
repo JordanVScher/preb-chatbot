d340570746494e905770fcf7731d9e5bc6a4f7b1
JordanVScher <jordanvscher@hotmail.com>
2019-02-12 18:48:04 -0200

    Update

--- app/handler.js
+++ app/handler.js
@@ -89,8 +89,7 @@ module.exports = async (context) => {
 				console.log('Recipient atual', await prepAPI.getRecipientPrep(context.session.user.id));
 				console.log(`Imprimindo os dados do perfil: \n${JSON.stringify(context.state.politicianData, undefined, 2)}`);
 				await context.setState({ is_eligible_for_research: null, is_part_of_research: null, finished_quiz: null });
-				// await context.setState({ dialog: 'greetings' });
-				await context.setState({ dialog: 'verConsulta' });
+				await context.setState({ dialog: 'greetings' });
 			} else if (context.state.whatWasTyped === process.env.TEST_KEYWORD) {
 				await context.setState({ selectedDate: 11 });
 				await context.setState({ dialog: 'setEventHour' });
