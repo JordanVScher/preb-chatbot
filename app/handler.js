27d315b3bda052e70624d60c80ce061c08278e9c
JordanVScher <jordanvscher@hotmail.com>
2019-06-20 19:33:51 -0400

    Add image on followup_messages

--- app/handler.js
+++ app/handler.js
@@ -99,6 +99,7 @@ module.exports = async (context) => {
 				await research.handleToken(context);
 			} else if (context.state.whatWasTyped.toLowerCase() === process.env.GET_PERFILDATA && process.env.ENV !== 'prod2') {
 				console.log('Deletamos o quiz?', await prepAPI.deleteQuizAnswer(context.session.user.id));
+				await context.setState({ user: await prepAPI.getRecipientPrep(context.session.user.id) });
 				await context.setState({ stoppedHalfway: false });
 				await context.setState({ startedQuiz: false, is_eligible_for_research: 0, is_target_audience: 0 });
 				await context.setState({ is_target_audience: false, is_prep: false, categoryQuestion: '' });
