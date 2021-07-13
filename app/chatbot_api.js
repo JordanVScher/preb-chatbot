15ed08b8f3cb0c1c107aff11e1dbb94c1f85ed51
Lucas Ansei <lucas.ansei@appcivico.com>
2021-07-13 10:54:55 -0300

    Using await on axios req

--- app/chatbot_api.js
+++ app/chatbot_api.js
@@ -9,7 +9,8 @@ const security_token = process.env.SECURITY_TOKEN_MA;
 const makeRequest = async (opt) => {
   try {
     if (opt.params) opt.params.security_token = security_token;
-    const result = axios(opt).then((res) => res).catch((err) => err.response);
+    // const result = axios(opt).then((res) => res).catch((err) => err.response);
+    const result = await axios(opt);
     console.log('req para MA');
     console.log(result);
     return handleRequestAnswer(result);
